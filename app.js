9dca90be6b596d46888d3f57d125e5713dbae90a
Sukhdeep Singh <sukhdeepsingh141414@gmail.com>
2025-06-07 21:55:35 +0530

    sign up

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const userSchema = require("./models/user.moduls.js")
+const userSchema = require("./models/user.moduls.js");
+// const bcrypt = require('bcryptjs');
 require("dotenv").config();
-
 
 const cors = require("cors");
 app.use(cors());
-
 
 const mongo_url = process.env.MONGO_URL;
 app.use(express.json());
@@ -15,31 +14,45 @@ app.get("/", (req, res) => {
   res.send("This is home api");
 });
 
-app.post("/register",async (req, res) => {
+app.post("/register", async (req, res) => {
   try {
-    const userCreated = await userSchema.create(req.body, console.log(req.body)
-    )
-    res.status(201).json({message: "User is created", user: userCreated})
-  
-    
+    const userCreated = await userSchema.create(
+      req.body,
+      console.log(req.body)
+    );
+    res.status(201).json({ message: "User is created", user: userCreated });
   } catch (error) {
-   res.status(500).json({message: error.message}) 
-  }
-
-}); 
-
-app.get("/user", async (req, res) => {
-  try {
-    const user = await userSchema.find()
-    res.status(200).json(user)
-  } catch (error) {
-       res.status(500).json({message: error.message}) 
-
+    res.status(500).json({ message: error.message });
   }
 });
 
-app.get("/signup", (req, res) => {
-  res.send("This is sign up page");
+app.get("/user", async (req, res) => {
+  try {
+    const user = await userSchema.find();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/signup", async (req, res) => {
+  try {
+    const check = await collection.findOne({ email: req.body.email });
+    if (!check) {
+      res.send("Email cannot found!");
+    }
+
+    const isPassword = await userSchema.collection.findOne({
+      password: req.body.password,
+    });
+    if (isPassword) {
+      res.send("login done");
+    } else {
+      req.send("Wrong password");
+    }
+  } catch (error) {
+    req.send("Wrong information!!!");
+  }
 });
 
 mongoose
