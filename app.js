addca3c58d92c6c0d4cb2ebb48bb00a70502ae76
Sukhdeep Singh <sukhdeepsingh141414@gmail.com>
2025-06-08 15:44:01 +0530

    update

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const userSchema = require("./models/user.moduls.js");
-// const bcrypt = require('bcryptjs');
+const User = require("./models/user.moduls.js"); // ✅ use consistent naming
+const bcrypt = require('bcryptjs');
 require("dotenv").config();
 
 const cors = require("cors");
 app.use(cors());
+app.use(express.json());
 
 const mongo_url = process.env.MONGO_URL;
-app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("This is home api");
 });
 
 app.post("/register", async (req, res) => {
   try {
-    const userCreated = await userSchema.create(
-      req.body,
-      console.log(req.body)
-    );
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const userCreated = await User.create({ name, email, password: hashedPassword });
+
     res.status(201).json({ message: "User is created", user: userCreated });
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -28,7 +33,7 @@ app.post("/register", async (req, res) => {
 
 app.get("/user", async (req, res) => {
   try {
-    const user = await userSchema.find();
+    const user = await User.find();
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -37,21 +42,27 @@ app.get("/user", async (req, res) => {
 
 app.post("/signup", async (req, res) => {
   try {
-    const check = await collection.findOne({ email: req.body.email });
-    if (!check) {
-      res.send("Email cannot found!");
+    const { email, password } = req.body;
+    console.log("Incoming data:", req.body);
+
+    if (!email || !password) {
+      return res.status(400).send("Email and password are required");
     }
 
-    const isPassword = await userSchema.collection.findOne({
-      password: req.body.password,
-    });
-    if (isPassword) {
-      res.send("login done");
-    } else {
-      req.send("Wrong password");
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).send("Email not found!");
     }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).send("Wrong password");
+    }
+
+    res.status(200).json({ message: "Login successful", user });
   } catch (error) {
-    req.send("Wrong information!!!");
+    console.error("Signup error:", error.message);
+    res.status(500).send("Something went wrong");
   }
 });
 
@@ -60,7 +71,7 @@ mongoose
   .then(() => {
     console.log("Connected to database");
     app.listen(5000, () => {
-      console.log("We are listing on port 5000");
+      console.log("We are listening on port 5000");
     });
   })
   .catch(() => {
--- models/user.moduls.js
+++ models/user.moduls.js
@@ -1,26 +1,9 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Please enter your name!!!"],
-        trim: true
-    },
-
-    email: {
-        type: String,
-        required: [true, "Please enter your email id"],
-        unique: true,
-        lowercase: true,
-        trim: true
-    },
-
-    password: {
-        type: String,
-        required: [true, "Please enter your password"],
-        minlength: [6, "Password must be at least 6 characters long"]
-    }
+  name: String,
+  email: String,
+  password: String,
 });
-
 
 module.exports = mongoose.model("User", userSchema);
